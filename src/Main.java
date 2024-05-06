f58afa4d2aa37fb3b906134b9a233928a4b200cf
jovalero <jrvalero2000@gmail.com>
2024-05-06 12:15:24 -0300

    Cambio jose

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		JOptionPane.showMessageDialog(null, "Aca reina ghami");
+		JOptionPane.showMessageDialog(null, "Aca reina jose");
 	}
 
 }
