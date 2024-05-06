2bf1634351f79ed5bec678a1a9cf36f222c12ee5
ghami20 <ghamiboxbox@gmail.com>
2024-05-06 12:06:08 -0300

    cambio en rama nueva

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		JOptionPane.showMessageDialog(null, "Los ositos cariñositos");
-		
-		JOptionPane.showMessageDialog(null, "Inicio de mi propia rama");
+		JOptionPane.showMessageDialog(null, "Aca reina ghami");
 	}
 
 }
